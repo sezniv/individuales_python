d8216fd1fb5e426111597be4b8419759a1ada76c
sezniv <sezniv@gmail.com>
2020-11-03 12:07:28 -0300

    Tarea numero 1

---
+++ prueba.py
@@ -0,0 +1,10 @@
+x = 3            
+f = 3.1415926               
+nombre = "Python"
+print(x)
+print(f)
+print(nombre)
+combinacion = nombre + " " + nombre
+print(combinacion)
+suma = f + x
+print(suma)
